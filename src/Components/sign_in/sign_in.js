b04a53b281b5879d0f23be23e94fc0b6c433d44d
mzmddi <110358757+mzmddi@users.noreply.github.com>
2024-04-13 14:16:35 -0400

    finished logic for signin - signin is 99% done (need set up routes for navigate('/Home')

--- src/Components/sign_in/sign_in.js
+++ src/Components/sign_in/sign_in.js
@@ -1,10 +1,56 @@
 import React from "react";
 import './sign_in.css';
+import { useState } from "react";
+//import {useNavigate} from 'react-router-dom';
+
+//text data to verify email and password
+import textData from '../Data/data.txt';
 
 //photos needed
 import image_right_side from '../../image/image_right_side_signin.png' ; 
 
+
 function Sign_in () {
+
+    //const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [fileContent, setFileContent] = useState(textData);
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+      };
+
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
+      };
+    
+    const handleSubmit = () => {
+
+        console.log('clicked button');
+        console.log('email: ' + email + ' password: ' + password);
+        console.log('fileContent: ' + fileContent);
+
+        fetch(fileContent)
+        .then(row => row.text())
+        .then(text => {
+            console.log('text inside then : ' + text);
+            console.log("text.includes(email + ' ' + password) : " + text.includes(email + ' ' + password))
+
+            if (text.includes(email + ' ' + password)) {
+            console.log('Email and password found ');
+            //navigate('/Home');
+            
+            } else {
+            console.log('Email and/or password not found.');
+            }
+
+
+            console.log('text:', text);
+        });
+    }
+
     return (
         <div className="full_screen">
             <div className="left_side_form_section">
@@ -12,21 +58,23 @@ function Sign_in () {
                 <div className="form_section">
                     <div className="welcome_back_message">Welcome Back!</div>
                     <label className='label_email' htmlFor="email">Email Address</label>
-                    <input className='input_email' type='email' name='email' placeholder="enter your email"/>
+                    <input className='input_email' type='email' name='email' placeholder="enter your email" value={email} onChange={handleChangeEmail}/>
                     <label className='label_password' htmlFor="password">Password</label>
-                    <input className='input_password' type='email' name='password' placeholder="enter your password"/>
-                    <div className="button_container"><button className="button_signin">Sign in</button></div>
-                    <div className="bottom_text">Don't have an account? Sign Up!</div>
+                    <input className='input_password' type='password' name='password' placeholder="enter your password" value={password} onChange={handleChangePassword}/>
+                    <div className="button_container"><button className="button_signin" onClick={handleSubmit}>Sign in</button></div>
+                    <div className="bottom_text" >Don't have an account? Sign Up!</div>
                 </div>
                 <div className="empty_space_holder">
 
                 </div>
+                
                 
             </div>
             <div className="right_side_image_section">
                 <img src={image_right_side} alt='right_side_image'/>
 
             </div>
+            
 
         </div>
     )
