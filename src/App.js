d434ecba7fb0e444f81d592d99be3f5bbb1fcb42
Ihana Fahmy <ihanafahmy@gmail.com>
2024-04-13 16:02:33 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 // import History from
-import TaskBox from "./Components/TaskBox/TaskBox";
 import "./App.css";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         {/* <Route path="/history" element={<History />} /> */}
-        <Route path="/testtask" element={<TaskBox />} />
       </Routes>
     </BrowserRouter>
   );
