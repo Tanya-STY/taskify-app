71a41a05a53964617aa0338c080008638534819f
Tanya So Tin Yan <tanya.sty99@gmail.com>
2024-04-14 01:43:59 -0400

    create all 4 forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import TaskForms from './Components/Task_forms/Task_forms';
+import TeamCreateTask from './Components/Task_forms/Team_Create_Task';
+import TeamViewTask from './Components/Task_forms/Team_View_Task';
+import UserCreateTask from './Components/Task_forms/User_Create_Task';
+import UserViewTask from './Components/Task_forms/User_View_Task';
 
 function App() {
   return (
     <div className="App">
-      <TaskForms />
+      {/* <TaskForms /> */}
+      {/* <TeamCreateTask/> */}
+      {/* <TeamViewTask/> */}
+      <UserCreateTask/>
+      {/* <UserViewTask/> */}
     </div>
   );
 }
