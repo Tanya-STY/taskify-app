caa0cd635335452abaf4b8a47b998237c04b0b91
Fadoua Doghmane <doghmanefadoua@gmail.com>
2024-04-14 01:09:37 -0400

    HomepageBanner isolé
    isolé le homepagebanner pour pouvoir le réutiliser dans les autres pages

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -4,39 +4,24 @@ import homepageimg from "../Image/homepage-image.jpg";
 import bellicon from "../Image/bell-icon.png";
 import "../Image/magnify-icon.png";
 import { Link } from "react-router-dom";
+import HomepageBanner from "../HomepageBanner/HomepageBanner";
 
 const Homepage = () => {
-  const currentDate = new Date();
-  const options = {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  };
-  const formattedDate = currentDate
-    .toLocaleDateString("en-US", options)
-    .replace(/,/g, "-");
-
   return (
-    <div className="container-homepage">
-      <div className="homepage-banner">
-        <img src={homepageimg} alt="banner" className="homepage-image" />
-        <p className="list-name-homepage">My To-Do List</p>
-        <p className="homepage-date">{formattedDate}</p>
-        <p className="homepage-welcome">Hello, Meredith! </p>
-        <p className="homepage-welcome-message">
-          Welcome back! Each day I will accomplish one thing on my list.
-        </p>
-        <Link to={"/"}>
-          <img src={bellicon} alt="bell icon" className="bell-icon-homepage" />
-        </Link>
-      </div>
-      <div className="homepage-search">
-        <input type="text" placeholder="Tasks, Tags, Title, etc." className="search-bar-homepage" />
-        <button className="new-task-button">New Task</button>
-      </div>
-      <div className="homepage-history">
-      <button className="history-button">History</button>
+    <div>
+      <HomepageBanner title="My To-Do List" /> {/* Pass the title prop here */}
+      <div className="container-homepage">
+        <div className="homepage-search">
+          <input
+            type="text"
+            placeholder="Tasks, Tags, Title, etc."
+            className="search-bar-homepage"
+          />
+          <button className="new-task-button">New Task</button>
+        </div>
+        <div className="homepage-history">
+          <button className="history-button">History</button>
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/HomepageBanner/HomepageBanner.js
@@ -0,0 +1,26 @@
+// HomepageBanner.js
+
+import React from "react";
+import homepageimg from "../Image/homepage-image.jpg";
+import bellicon from "../Image/bell-icon.png";
+import { Link } from "react-router-dom";
+import "./HomepageBanner.css";
+
+const HomepageBanner = ({ formattedDate, title }) => {
+  return (
+    <div className="homepage-banner">
+      <img src={homepageimg} alt="banner" className="homepage-image" />
+      <p className="list-name-homepage">{title}</p> {/* Use the title prop here */}
+      <p className="homepage-date">{formattedDate}</p>
+      <p className="homepage-welcome">Hello, Meredith! </p>
+      <p className="homepage-welcome-message">
+        Welcome back! Each day I will accomplish one thing on my list.
+      </p>
+      <Link to={"/"}>
+        <img src={bellicon} alt="bell icon" className="bell-icon-homepage" />
+      </Link>
+    </div>
+  );
+};
+
+export default HomepageBanner;
