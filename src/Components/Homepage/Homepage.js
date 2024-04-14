c884e1cfa8eb2aac207fe13b73786febb1c354ed
Ihana Fahmy <ihanafahmy@gmail.com>
2024-04-14 01:58:14 -0400

    Finished the main dashboard and the tasks function

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -19,7 +19,7 @@ const Homepage = () => {
     .replace(/,/g, "-");
 
   return (
-    <div className="container-homepage">
+    <>
       <div className="homepage-banner">
         <img src={homepageimg} alt="banner" className="homepage-image" />
         <p className="list-name-homepage">My To-Do List</p>
@@ -39,35 +39,54 @@ const Homepage = () => {
           className="search-bar-homepage"
         />
         <button className="new-task-button">New Task</button>
-      </div>
-      <div>
-        <Taskbox title="Gym Workout" description="Working out with friends!" tag="workout" date="15 November 2024" priority="Low" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Gym Workout" description="Working out with friends!" tag="workout" date="15 November 2024" priority="Low" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Gym Workout" description="Working out with friends!" tag="workout" date="15 November 2024" priority="Low" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Gym Workout" description="Working out with friends!" tag="workout" date="15 November 2024" priority="Low" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Gym Workout" description="Working out with friends!" tag="workout" date="15 November 2024" priority="Low" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Gym Workout" description="Working out with friends!" tag="workout" date="15 November 2024" priority="Low" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Gym Workout" description="Working out with friends!" tag="workout" date="15 November 2024" priority="Low" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-        <Taskbox title="Finish Studying" description="Worst day of my life:((" tag="study sesh" date="13 April 2024" priority="High" />
-
-      </div>
-      <div className="homepage-history">
         <button className="history-button">History</button>
       </div>
-    </div>
+      <div className="tasks-homepage">
+        <Taskbox
+          title="React Tutorial"
+          description="Learn React Fundamentals"
+          tag="Learning"
+          date="15 November 2024"
+          priority="Medium"
+        />
+        <Taskbox
+          title="Write Blog Post"
+          description="Benefits of Meditation"
+          tag="BlogPost"
+          date="9 April 2024"
+          priority="High"
+        />
+        <Taskbox
+          title="Family Zoom"
+          description="Family Trip for Summer"
+          tag="Meeting"
+          date="13 April 2024"
+          priority="Medium"
+        />
+        <Taskbox
+          title="Gym Workout"
+          description="Strength Training Session"
+          tag="Workout"
+          date="15 July 2024"
+          priority="Low"
+        />
+        <Taskbox
+          title="Call Mom"
+          description="Catch Up on Family News"
+          tag="Family"
+          date="28 September 2024"
+          priority="Medium"
+        />
+        <Taskbox
+          title="Finish Studying"
+          description="WE CAN DO THIS!!!"
+          tag="StudySesh"
+          date="20 April 2024"
+          priority="High"
+        />
+
+      </div>
+    </>
   );
 };
 
